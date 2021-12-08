b334f143ff5567149702055753ef3d051d431170
1000yj <37067204+cheonyeji@users.noreply.github.com>
2021-12-08 18:20:44 +0900

    Optional Props

--- react-master/src/Circle.tsx
+++ react-master/src/Circle.tsx
@@ -2,19 +2,25 @@ import styled from 'styled-components';
 
 interface ContainerProps {
     bgColor: string;
+    borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
     width:200px;
     height: 200px;
     background-color: ${props => props.bgColor};
+    border: 1px solid ${props=>props.borderColor};
     border-radius: 100px;
 `;
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
+    text?: string;
 }
-function Circle({bgColor}:CircleProps) {
-    return <Container bgColor={bgColor}/>
+function Circle({bgColor, borderColor, text="default text"}:CircleProps) {
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text}
+    </Container>
 }
 
 export default Circle;
