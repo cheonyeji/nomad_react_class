46bf231bbf452dc8cd69ff645b1817893390a205
1000yj <37067204+cheonyeji@users.noreply.github.com>
2021-12-08 15:20:41 +0900

    Add Theme
    - styled-components : ThemeProvider
    - deliver color object to child components using ThemeProvider

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,49 +1,20 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
-const rotateAni = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${rotateAni} 1s linear infinite;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  ${Emoji}:hover {
-    font-size: 48px;
-  }
-`;
-
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>🤩</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
