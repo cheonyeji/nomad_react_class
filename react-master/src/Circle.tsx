2efdc31c2eb46fc5681e394a16f9b71eaf8c3716
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-03 17:04:25 +0900

    [style] changed tab style ... etc
    vs code default formatting으로 변경

--- react-master/src/Circle.tsx
+++ react-master/src/Circle.tsx
@@ -1,29 +1,30 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
 interface ContainerProps {
-    bgColor: string;
-    borderColor: string;
+  bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
-    width:200px;
-    height: 200px;
-    background-color: ${props => props.bgColor};
-    border: 1px solid ${props=>props.borderColor};
-    border-radius: 100px;
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border: 2px solid ${(props) => props.borderColor};
+  border-radius: 100px;
 `;
 interface CircleProps {
-    bgColor: string;
-    borderColor?: string;
-    text?: string;
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
+
 function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
-    const [value, setValue] = useState("");
-    // const [changeType, setChangeType] = useState<number | string>();
-    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
-        {text}
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
     </Container>
+  );
 }
 
 export default Circle;
