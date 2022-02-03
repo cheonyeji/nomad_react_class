772ce5d57e90e4ee984876115da7ec52112c82af
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-03 19:42:58 +0900

    Add theme option
    - dark / light mode

--- react-master/src/index.tsx
+++ react-master/src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { lightTheme } from "./theme";
+import { blackTheme, lightTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={blackTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
