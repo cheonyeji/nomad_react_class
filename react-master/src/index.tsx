09e624e2be323eb5809933644ca496ed00b4c936
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-07 06:07:15 +0900

    #5.5 Set up

--- react-master/src/index.tsx
+++ react-master/src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { blackTheme, lightTheme } from "./theme";
+import { darkTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={blackTheme}>
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
