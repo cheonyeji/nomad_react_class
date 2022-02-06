b88e6ce99ec714ce23dd48f1a5f81e8755a41e12
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-07 06:31:59 +0900

    #5,6 react-hook-form
    - Good for time-consuming works w/ react form (ex. onChange, onBlur, validation, multiple states... -> useForm() )

--- react-master/src/ToDoList.tsx
+++ react-master/src/ToDoList.tsx
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+// function ToDoList() {
+//   const [todo, setTodo] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(todo);
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder="Write a to do" />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [todo, setTodo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  };
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="Write a to do" />
+      <form>
+        <input {...register("todo")} placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
