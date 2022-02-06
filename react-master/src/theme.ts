338592dc88475b357296a83a81f7e1fdc7c2ae14
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-07 05:47:17 +0900

    Add dark/light mode

--- react-master/src/theme.ts
+++ react-master/src/theme.ts
@@ -1,7 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
+export const darktheme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "#f5f6fa",
+  textColor: "white",
   accentColor: "#4cd137",
+  cardBgColor: "transparent",
+};
+
+export const lighttheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "black",
+  accentColor: "#9c88ff",
+  cardBgColor: "white",
 };
