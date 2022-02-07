7024f98b0e4e71f05275ff1de770ce5c0b22facd
1000yj <37067204+cheonyeji@users.noreply.github.com>
2022-02-07 16:23:23 +0900

    #5.9 custom validation
    - Add interface of form data (IForm)
    - setError from useForm hook : custom validation
    - validate option : can be async function(w/ backend)
    - For practice, add multiple input

--- react-master/src/ToDoList.tsx
+++ react-master/src/ToDoList.tsx
@@ -22,23 +22,99 @@
 
 import { useForm } from "react-hook-form";
 
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
+
 function ToDoList() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
-  const onValid = (data: any) => {
-    console.log(data);
+    setError,
+  } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraError", { message: "server offine" });
   };
+  console.log(errors);
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
         <input
-          {...register("todo", { required: "To do is required" })}
-          placeholder="Write a to do"
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="email"
         />
-        <span>{errors?.todo?.message}</span>
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noYeji: (inputedVal) =>
+                inputedVal.includes("yeji") ? "no yeji allowed" : true,
+              noJihye: (inputedVal) =>
+                inputedVal.includes("jihye") ? "no jihye allowed" : true,
+            },
+          })}
+          placeholder="firstName"
+        />
+        <span>{errors?.firstName?.message}</span>
+        <input
+          {...register("lastName", { required: "write here" })}
+          placeholder="lastName"
+        />
+        <span>{errors?.lastName?.message}</span>
+        <input
+          {...register("username", {
+            required: "write here",
+            minLength: {
+              value: 5,
+              message: "Too short username",
+            },
+          })}
+          placeholder="username"
+        />
+        <span>{errors?.username?.message}</span>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Password is too short",
+            },
+          })}
+          placeholder="password"
+        />
+        <span>{errors?.password?.message}</span>
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: 5,
+          })}
+          placeholder="password1"
+        />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
